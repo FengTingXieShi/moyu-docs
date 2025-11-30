cd08d67f4227472ee0e85a1287026fa036bd35e4
𝓍𝒾𝑒𝓈𝒽𝒾. <3773793283@qq.com>
2025-11-30 11:48:34 +0800

    Add resend-config.js with API key and URL

---
+++ js/resend-config.js
@@ -0,0 +1,4 @@
+const RESEND_API_KEY = 're_3W7kQD4C_27oK5xCgctstQYUGJ6rRxVYf';
+const FRONTEND_VERIFY_URL = 'https://fengtingxieshi.github.io/moyu-docs/verify-email';
+
+export { RESEND_API_KEY, FRONTEND_VERIFY_URL };
